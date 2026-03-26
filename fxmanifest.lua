a15aa00125b17c95d2627dc8a35d0ef169feb300
a1064074535-droid <a1064074535@gmail.com>
2026-03-26 07:13:07 +0300

    Add fxmanifest for Toyota Hilux 2025 script

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Claude AI'
+description 'Toyota Hilux 2025 - Professional Vehicle Script'
+version '1.0.0'
+
+shared_scripts {
+      'config.lua',
+      'locales.lua'
+}
+
+client_scripts {
+      'client.lua'
+}
+
+server_scripts {
+      'server.lua'
+}
